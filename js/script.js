f999be72dedd2bf246b12638ee8cdceb54e46df3
antonysan <tonik32@gmail.com>
2017-11-29 13:38:18 +0300

    Переделана карта, добавлены скрипты и маркер на карту

---
+++ js/script.js
@@ -0,0 +1,22 @@
+   ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938723, 30.328377],
+            zoom: 16
+        }),
+
+        myPlacemark1 = new ymaps.Placemark([59.938631, 30.323055], {
+            balloonContent: 'Интернет магазин Gllacy'
+        }, {
+            iconLayout: 'default#image',
+            iconImageClipRect: [[0,0], [82, 142]],
+            iconImageHref: 'img/pin.png',
+            iconImageSize: [60, 90],
+            iconImageOffset: [-40, -90],
+        });
+
+    myMap.geoObjects.add(myPlacemark1)
+        .add(myPlacemark2)
+        .add(myPlacemark3);
+}
