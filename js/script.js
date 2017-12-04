9780fe095c0de2499b4429ed6934b8c5f9e8502e
antonysan <tonik32@gmail.com>
2017-12-04 15:59:20 +0300

    Собирает все скрипты в один файл

--- js/script.js
+++ js/script.js
@@ -19,4 +19,17 @@ function init () {
     myMap.geoObjects.add(myPlacemark1)
         .add(myPlacemark2)
         .add(myPlacemark3);
-}
+};
+var link = document.querySelector(".callback-button");
+var modal = document.querySelector(".popup-callback");
+var modalClose = document.querySelector(".button-close");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("popup-callback-show");
+});
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("popup-callback-show");
+});
